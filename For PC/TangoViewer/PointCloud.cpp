7b58db8fa10a1daaef913157958bb0bcc1d7051e
kukdh1 <kukdh1@gmail.com>
2015-10-15 00:54:33 +0900

    Plane Segmentation

--- For PC/TangoViewer/PointCloud.cpp
+++ For PC/TangoViewer/PointCloud.cpp
@@ -5,14 +5,26 @@ namespace kukdh1
 	PointCloud::PointCloud()
 	{
 		pclPointCloud = pcl::PointCloud<pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
+		pclPointCloudUnordered = pcl::PointCloud<pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
 	}
 
 	PointCloud::~PointCloud()
 	{
+		size_t stSize;
 
+		pclPointCloud->clear();
+		pclPointCloudUnordered->clear();
+
+		stSize = pclPlaneCoeffs.size();
+		for (size_t i = 0; i < stSize; i++)
+			pclPlaneCoeffs.at(i).reset();
+
+		stSize = pclPlanePoints.size();
+		for (size_t i = 0; i < stSize; i++)
+			pclPlanePoints.at(i)->clear();
 	}
 
-	void PointCloud::DrawOnGLWindow()
+	void PointCloud::DrawOnGLWindow(BOOL bDrawPlanes)
 	{
 		size_t stSize;
 
@@ -23,11 +35,11 @@ namespace kukdh1
 		glPointSize(3.0f);
 		glBegin(GL_POINTS);
 
-		stSize = pclPointCloud->size();
+		stSize = pclPointCloudUnordered->size();
 
 		for (unsigned int i = 0; i < stSize; i++)
 		{
-			pcl::PointXYZRGB &pclPointXYZRGB = pclPointCloud->at(i);
+			pcl::PointXYZRGB &pclPointXYZRGB = pclPointCloudUnordered->at(i);
 
 			glColor3ub(pclPointXYZRGB.b, pclPointXYZRGB.g, pclPointXYZRGB.r);
 
@@ -36,7 +48,43 @@ namespace kukdh1
 
 		glEnd();
 
+		if (bDrawPlanes)
+		{
+			size_t stPlane;
+			size_t stPoint;
+
+			glBegin(GL_POINTS);
+
+			stPlane = pclPlanePoints.size();
+
+			for (size_t i = 0; i < stPlane; i++)
+			{
+				stPoint = pclPlanePoints.at(i)->size();
+
+				glColor3ub(255 * (i % 3 == 0 ? 1 : 0), 255 * (i % 3 == 1 ? 1 : 0), 255 * (i % 3 == 2 ? 1 : 0));
+
+				for (size_t j = 0; j < stPoint; j++)
+				{
+					pcl::PointXYZRGB &pclPointXYZRGB = pclPlanePoints.at(i)->at(j);
+
+					glVertex3f(pclPointXYZRGB.x + (i + 1) * 1, pclPointXYZRGB.y, pclPointXYZRGB.z);
+				}
+			}
+
+			glEnd();
+		}
+
 		glPopMatrix();
+	}
+
+	void * PointCloud::operator new(size_t i)
+	{
+		return _aligned_malloc(i, 16);
+	}
+
+	void PointCloud::operator delete(void *ptr)
+	{
+		_aligned_free(ptr);
 	}
 
 	void PointCloud::AdjustTransformMatrix(glm::mat4 &m4AdjustMatrix)
@@ -49,45 +97,100 @@ namespace kukdh1
 		pcl::PointCloud<pcl::PointXYZRGB>::Ptr pclOutput(new pcl::PointCloud<pcl::PointXYZRGB>);
 		pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> pclSOR;
 
-		pclSOR.setInputCloud(pclPointCloud);
+		pclSOR.setInputCloud(pclPointCloudUnordered);
 		pclSOR.setMeanK(SOR_MEAN_K);
 		pclSOR.setStddevMulThresh(SOR_STDDEV_MUL_THRES);
 		pclSOR.filter(*pclOutput);
 
-		pclPointCloud = pclOutput;
+		pclPointCloudUnordered = pclOutput;
 	}
-	std::vector<pcl::PlanarRegion<pcl::PointXYZRGB>, Eigen::aligned_allocator<pcl::PlanarRegion<pcl::PointXYZRGB>>> pclRegion;
 
-	void PointCloud::ExecutePlaneSegmentation()
+	size_t PointCloud::ExecutePlaneSegmentation()
 	{
+		/*
 		pcl::OrganizedMultiPlaneSegmentation<pcl::PointXYZRGB, pcl::Normal, pcl::Label> pclOMPS;
-		pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> pclNE;
-		pcl::search::KdTree<pcl::PointXYZRGB>::Ptr pclKdTree(new pcl::search::KdTree<pcl::PointXYZRGB>());
+		pcl::IntegralImageNormalEstimation<pcl::PointXYZRGB, pcl::Normal> pclIINE;
 		pcl::PointCloud<pcl::Normal>::Ptr pclNormalCloud(new pcl::PointCloud<pcl::Normal>);
 
 		//Estimate Normals
-		pclNE.setInputCloud(pclPointCloud);
-		pclNE.setSearchMethod(pclKdTree);
-		pclNE.setRadiusSearch(NE_RADIUS_SEARCH);
-		pclNE.compute(*pclNormalCloud);
+		pclIINE.setNormalEstimationMethod(pclIINE.IINE_METHOD);
+		pclIINE.setMaxDepthChangeFactor(IINE_MAX_DEPTH_FACTOR);
+		pclIINE.setNormalSmoothingSize(IINE_SMOOTH_FACTOR);
+		pclIINE.setInputCloud(pclPointCloud);
+		pclIINE.compute(*pclNormalCloud);
 
 		//Segment Planes
 		pclOMPS.setMinInliers(OMPS_MINIMAL_INLIERS_PLANE);
 		pclOMPS.setAngularThreshold(OMPS_ANGULAR_THRESHOLD);
 		pclOMPS.setDistanceThreshold(OMPS_DISTANCE_THRESHOLD);
+		pclOMPS.setMaximumCurvature(OMPS_MAXIMUM_CULVATURE);
 		pclOMPS.setInputNormals(pclNormalCloud);
 		pclOMPS.setInputCloud(pclPointCloud);
-		pclOMPS.segmentAndRefine(pclRegion);
+		pclOMPS.segment(pclRegion);
+		
+		return pclRegion.size();
+		*/
+
+		//http://www.pointclouds.org/documentation/tutorials/extract_indices.php
+
+		pcl::ModelCoefficients::Ptr pclModelCoeff(new pcl::ModelCoefficients());
+		pcl::PointIndices::Ptr pclInliers(new pcl::PointIndices());
+		pcl::SACSegmentation<pcl::PointXYZRGB> pclSACSeg;
+		pcl::ExtractIndices<pcl::PointXYZRGB> pclExtract;
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr pclCloud(new pcl::PointCloud<pcl::PointXYZRGB>(*pclPointCloudUnordered));
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr pclCloudF(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+		size_t stPointCount;
+
+		pclSACSeg.setOptimizeCoefficients(TRUE);
+		pclSACSeg.setModelType(pcl::SACMODEL_PLANE);
+		pclSACSeg.setMethodType(pcl::SAC_RANSAC);
+		pclSACSeg.setMaxIterations(SAC_SEG_MAX_ITERATION);
+		pclSACSeg.setDistanceThreshold(SAC_SEG_DISTANCE_THRESHOLD);
+
+		stPointCount = pclCloud->size();
+
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr pclPlaneIndices;
+
+		while (pclCloud->size() > SAC_SEG_RATIO * stPointCount)
+		{
+			size_t stIndices;
+
+			pclPlaneIndices = pcl::PointCloud<pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+			pclSACSeg.setInputCloud(pclCloud);
+			pclSACSeg.segment(*pclInliers, *pclModelCoeff);
+
+			stIndices = pclInliers->indices.size();
+
+			if (stIndices == 0)
+				break;
+
+			pclExtract.setInputCloud(pclCloud);
+			pclExtract.setIndices(pclInliers);
+			pclExtract.setNegative(FALSE);
+			pclExtract.filter(*pclPlaneIndices);
+			pclExtract.setNegative(TRUE);
+			pclExtract.filter(*pclCloudF);
+
+			pclCloud.swap(pclCloudF);
+
+			//Save
+			pclPlaneCoeffs.push_back(pclModelCoeff);
+			pclPlanePoints.push_back(pclPlaneIndices);
+
+			pclModelCoeff = pcl::ModelCoefficients::Ptr(new pcl::ModelCoefficients());
+		}
+
+		return pclPlaneCoeffs.size();
 	}
 
 	BOOL PointCloud::FromFile(WCHAR * pszFilePath)
 	{
 		HANDLE hFile;
 		DWORD dwRead;
-		Point *ppPoints;
+		Pointij *ppPointij;
 		glm::mat4 m4Temp;
-		glm::vec4 v4Temp;
-		pcl::PointXYZRGB pclPointXYZRGB;
 		unsigned int uiPointCount;
 
 		hFile = CreateFile(pszFilePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -103,26 +206,57 @@ namespace kukdh1
 				ReadFile(hFile, fMatrix, 16 * 4, &dwRead, NULL);
 				memcpy(glm::value_ptr(m4Temp), fMatrix, 16 * 4);
 
-				ppPoints = (Point *)calloc(uiPointCount, sizeof(Point));
+				ppPointij = (Pointij *)calloc(uiPointCount, sizeof(Pointij));
 
-				if (ppPoints)
+				if (ppPointij)
 				{
-					ReadFile(hFile, ppPoints, 16 * uiPointCount, &dwRead, NULL);
+					ReadFile(hFile, ppPointij, sizeof(Pointij) * uiPointCount, &dwRead, NULL);
 
-					v4Temp.w = 1;
+					//Initiate Point Cloud Class
+					pclPointCloud->clear();
+					pclPointCloud->width = TANGO_IMAGE_WIDTH;
+					pclPointCloud->height = TANGO_IMAGE_HEIGHT;
+					pclPointCloud->resize(TANGO_IMAGE_WIDTH * TANGO_IMAGE_HEIGHT);
 
-					for (unsigned int i = 0; i < uiPointCount; i++)
+					pclPointCloudUnordered->clear();
+					pclPointCloudUnordered->width = uiPointCount;
+					pclPointCloudUnordered->height = 1;
+					pclPointCloudUnordered->resize(uiPointCount);
+
+					//Set NAN
+					concurrency::parallel_for(size_t(0), (size_t)(TANGO_IMAGE_WIDTH * TANGO_IMAGE_HEIGHT), [&](size_t i)
 					{
-						memcpy(glm::value_ptr(v4Temp), ppPoints + i, 12);
-						v4Temp = m4Temp * kOpengGL_T_Depth * v4Temp;
-						
-						pclPointXYZRGB.x = v4Temp.x;
-						pclPointXYZRGB.y = v4Temp.y;
-						pclPointXYZRGB.z = v4Temp.z;
-						pclPointXYZRGB.rgba = ppPoints[i].c;
+						pclPointCloud->at(i).x = NAN;
+						pclPointCloud->at(i).y = NAN;
+						pclPointCloud->at(i).z = NAN;
+					});
 
-						pclPointCloud->push_back(pcl::PointXYZRGB(pclPointXYZRGB));
-					}
+					//Set Read Point Cloud
+					concurrency::parallel_for(size_t(0), uiPointCount, [&](size_t i)
+					{
+						glm::vec4 v4Temp;
+						size_t idx;
+						Pointij *ppNow;
+
+						ppNow = ppPointij + i;
+						v4Temp.w = 1;
+						idx = ppNow->ijs.x + ppNow->ijs.y * TANGO_IMAGE_WIDTH;
+
+						memcpy(glm::value_ptr(v4Temp), ppNow, 12);
+						v4Temp = m4Temp * kOpengGL_T_Depth * v4Temp;
+
+						pclPointCloud->at(idx).x = v4Temp.x;
+						pclPointCloud->at(idx).y = v4Temp.y;
+						pclPointCloud->at(idx).z = v4Temp.z;
+						pclPointCloud->at(idx).rgba = ppNow->c;
+
+						pclPointCloudUnordered->at(i).x = v4Temp.x;
+						pclPointCloudUnordered->at(i).y = v4Temp.y;
+						pclPointCloudUnordered->at(i).z = v4Temp.z;
+						pclPointCloudUnordered->at(i).rgba = ppNow->c;
+					});
+
+					free(ppPointij);
 				}
 			}
 
--- For PC/TangoViewer/PointCloud.h
+++ For PC/TangoViewer/PointCloud.h
@@ -7,27 +7,43 @@
 
 #include <Windows.h>
 #include <gl/GL.h>
+#include <ppl.h>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/statistical_outlier_removal.h>
-#include <pcl/features/normal_3d.h>
+#include <pcl/features/integral_image_normal.h>
 #include <pcl/segmentation/organized_multi_plane_segmentation.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
 
 #include "glm/mat4x4.hpp"
 #include "glm/vec4.hpp"
 #include "glm/gtc/type_ptr.hpp"
 #include "glm/gtc/matrix_inverse.hpp"
 
+#define TANGO_IMAGE_WIDTH		1280
+#define TANGO_IMAGE_HEIGHT		720
+
+#define SAC_SEG_MAX_ITERATION			1000
+#define SAC_SEG_DISTANCE_THRESHOLD		0.05
+#define SAC_SEG_RATIO					0.1
+
 #define RADIAN_PER_DEGREE			0.017453f
 #define OMPS_DISTANCE_THRESHOLD		0.02
-#define OMPS_MINIMAL_INLIERS_PLANE	500
+#define OMPS_MINIMAL_INLIERS_PLANE	2000
 #define OMPS_ANGULAR_THRESHOLD		RADIAN_PER_DEGREE * 2
+#define OMPS_MAXIMUM_CULVATURE		0x002
+
+#define IINE_METHOD				AVERAGE_3D_GRADIENT
+#define IINE_MAX_DEPTH_FACTOR	0.02f
+#define IINE_SMOOTH_FACTOR		10.0f
+
 
 #define SOR_MEAN_K				50
 #define SOR_STDDEV_MUL_THRES	1.0
-
-#define NE_RADIUS_SEARCH		0.03
 
 
 const glm::mat4 kOpengGL_T_Depth =
@@ -47,6 +63,22 @@ namespace kukdh1
 		COLORREF c;
 	};
 
+	struct Pointij
+	{
+		float x;
+		float y;
+		float z;
+		COLORREF c;
+		union
+		{
+			unsigned int ij;
+			struct {
+				unsigned short x;
+				unsigned short y;
+			};
+		} ijs;
+	};
+
 	/* PointCloud class
 	 *  It contains PointCloud at such time
 	 */
@@ -56,16 +88,23 @@ namespace kukdh1
 			glm::mat4 m4Adjust;
 
 			pcl::PointCloud<pcl::PointXYZRGB>::Ptr pclPointCloud;
+			pcl::PointCloud<pcl::PointXYZRGB>::Ptr pclPointCloudUnordered;
+		//	std::vector<pcl::PlanarRegion<pcl::PointXYZRGB>, Eigen::aligned_allocator<pcl::PlanarRegion<pcl::PointXYZRGB>>> pclRegion;
+			std::vector<pcl::ModelCoefficients::Ptr> pclPlaneCoeffs;
+			std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> pclPlanePoints;
 
 		public:
 			PointCloud();
 			~PointCloud();
 
-			void DrawOnGLWindow();
+			void DrawOnGLWindow(BOOL bDrawPlanes);
 			
 			void AdjustTransformMatrix(glm::mat4 &m4AdjustMatrix);
 			void ApplyStaticalOutlierRemoveFilter();
-			void ExecutePlaneSegmentation();
+			size_t ExecutePlaneSegmentation();
+
+			static void * operator new(size_t);
+			static void operator delete(void *);
 
 			BOOL FromFile(WCHAR *pszFilePath);
 	};
